7f8898a13a9323b1f5bc05ca97481f439b7c8b4f
Gregory Klein <gregoryhklein@gmail.com>
2015-01-17 23:19:08 -0500

    Initial Commit

---
+++ SoundBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundBoard
+//
+//  Created by Gregory Klein on 1/17/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SoundBoard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SoundBoard
+//
+//  Created by Gregory Klein on 1/17/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+   @autoreleasepool {
+       return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+   }
+}
