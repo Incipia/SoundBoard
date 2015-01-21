ba70fe78021b0426511f6e8b849dcf2f75a51d16
Gregory Klein <gregoryhklein@gmail.com>
2015-01-21 17:03:57 -0500

    Added category for setting letter on text layer

---
+++ CustomKeyboard/CATextLayer+SetLetter.h
@@ -0,0 +1,13 @@
+//
+//  CATextLayer+SetLetter.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/21/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface CATextLayer_SetLetter : CATextLayer
+
+@end
---
+++ CustomKeyboard/CATextLayer+SetLetter.m
@@ -0,0 +1,13 @@
+//
+//  CATextLayer+SetLetter.m
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/21/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "CATextLayer+SetLetter.h"
+
+@implementation CATextLayer_SetLetter
+
+@end
