9e70f3c17d091b032a0c5685f9e19a180faece2c
Gregory Klein <gregoryhklein@gmail.com>
2015-01-21 13:26:19 -0500

    Added new files

---
+++ CustomKeyboard/LetterView.h
@@ -0,0 +1,13 @@
+//
+//  LetterView.h
+//  SoundBoard
+//
+//  Created by Gregory Klein on 1/21/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LetterView : UIView
+
+@end
---
+++ CustomKeyboard/LetterView.m
@@ -0,0 +1,21 @@
+//
+//  LetterView.m
+//  SoundBoard
+//
+//  Created by Gregory Klein on 1/21/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "LetterView.h"
+
+@implementation LetterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
