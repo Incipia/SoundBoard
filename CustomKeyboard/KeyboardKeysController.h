02baab58efdbc23894cc273a65d5613230d20547
Gregory Klein <gregoryhklein@gmail.com>
2015-01-22 23:38:46 -0500

    Started implementing keyboard keys controller

--- CustomKeyboard/KeyboardKeysController.h
+++ CustomKeyboard/KeyboardKeysController.h
@@ -10,4 +10,6 @@
 
 @interface KeyboardKeysController : UIViewController
 
++ (instancetype)controller;
+
 @end
--- CustomKeyboard/KeyboardKeysController.m
+++ CustomKeyboard/KeyboardKeysController.m
@@ -7,31 +7,77 @@
 //
 
 #import "KeyboardKeysController.h"
+#import "LetterView.h"
 
 @interface KeyboardKeysController ()
-
+@property (nonatomic) UIView* topLettersContainer;
+@property (nonatomic) UIView* middleLettersContainer;
+@property (nonatomic) UIView* bottomLettersContainer;
+@property (nonatomic) UIView* bottomKeysContainer;
+@property (nonatomic, readonly) NSArray* containerViews;
 @end
 
 @implementation KeyboardKeysController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+#pragma mark - Init
+- (instancetype)init
+{
+   if (self = [super init])
+   {
+   }
+   return self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Class Init
++ (instancetype)controller
+{
+   return [[KeyboardKeysController alloc] init];
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - Setup
+- (void)setupContainerViews
+{
+   self.topLettersContainer = [UIView new];
+   self.middleLettersContainer = [UIView new];
+   self.bottomLettersContainer = [UIView new];
+   self.bottomKeysContainer = [UIView new];
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+   for (UIView* containerView in self.containerViews)
+   {
+      [self.view addSubview:containerView];
+   }
 }
-*/
+
+#pragma mark - Lifecycle
+- (void)viewDidLoad
+{
+   [super viewDidLoad];
+   [self setupContainerViews];
+}
+
+- (void)viewDidLayoutSubviews
+{
+   [self updateContainerViewFrames];
+}
+
+#pragma mark - Update
+- (void)updateContainerViewFrames
+{
+   CGFloat containerViewWidth = CGRectGetWidth(self.view.bounds);
+   CGFloat containerViewHeight = CGRectGetHeight(self.view.bounds) / self.containerViews.count;
+   CGRect currentContainerViewFrame = CGRectMake(0, 0, containerViewWidth, containerViewHeight);
+
+   for (UIView* containerView in self.containerViews)
+   {
+      containerView.frame = currentContainerViewFrame;
+      currentContainerViewFrame.origin.y += containerViewHeight;
+   }
+}
+
+#pragma mark - Property Overrides
+- (NSArray*)containerViews
+{
+   return @[self.topLettersContainer, self.middleLettersContainer, self.bottomLettersContainer, self.bottomKeysContainer];
+}
 
 @end
