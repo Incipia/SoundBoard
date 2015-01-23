8ce7210acf7231450d123e3e785d0488db0dd4fa
Gregory Klein <gregoryhklein@gmail.com>
2015-01-23 02:43:14 -0500

    Added files for LetterViewCollection

---
+++ CustomKeyboard/LetterViewCollection.h
@@ -0,0 +1,13 @@
+//
+//  LetterViewCollection.h
+//  SoundBoard
+//
+//  Created by Gregory Klein on 1/23/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LetterViewCollection : UIView
+
+@end
---
+++ CustomKeyboard/LetterViewCollection.m
@@ -0,0 +1,21 @@
+//
+//  LetterViewCollection.m
+//  SoundBoard
+//
+//  Created by Gregory Klein on 1/23/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "LetterViewCollection.h"
+
+@implementation LetterViewCollection
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
