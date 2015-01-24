62258a2282600a11cea4455d6dac9ba93c26364a
Gregory Klein <gregoryhklein@gmail.com>
2015-01-23 21:42:16 -0500

    Added base class for functional key controllers

---
+++ CustomKeyboard/FunctionalKeyController.h
@@ -0,0 +1,13 @@
+//
+//  FunctionalKeyController.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/23/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FunctionalKeyController : UIViewController
+
+@end
