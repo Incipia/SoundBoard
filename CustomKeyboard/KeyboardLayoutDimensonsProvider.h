218c5a09c77db7e6c990b37c048ce45d931e6991
Gregory Klein <gregoryhklein@gmail.com>
2015-01-25 13:50:01 -0500

    Added files for KeyboardLayoutDimensionsProvider

---
+++ CustomKeyboard/KeyboardLayoutDimensonsProvider.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardLayoutDimensonsProvider.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/25/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeyboardLayoutDimensonsProvider : NSObject
+
+@end
---
+++ CustomKeyboard/KeyboardLayoutDimensonsProvider.m
@@ -0,0 +1,13 @@
+//
+//  KeyboardLayoutDimensonsProvider.m
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/25/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "KeyboardLayoutDimensonsProvider.h"
+
+@implementation KeyboardLayoutDimensonsProvider
+
+@end
