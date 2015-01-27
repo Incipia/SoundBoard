4f4094df3347349f51bac62bb210264ab8e959f1
Gregory Klein <gregoryhklein@gmail.com>
2015-01-26 23:42:45 -0500

    Added file to wrap key frame text dictionary

---
+++ CustomKeyboard/KeyboardKeyFrameTextMap.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardKeyFrameTextMap.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/26/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeyboardKeyFrameTextMap : NSObject
+
+@end
---
+++ CustomKeyboard/KeyboardKeyFrameTextMap.m
@@ -0,0 +1,13 @@
+//
+//  KeyboardKeyFrameTextMap.m
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/26/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "KeyboardKeyFrameTextMap.h"
+
+@implementation KeyboardKeyFrameTextMap
+
+@end
