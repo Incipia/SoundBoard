6e82d9e5ae140483bfa66f6bcf022470c258629b
Gregory Klein <gregoryhklein@gmail.com>
2015-01-28 02:03:25 -0500

    Making key view look WAY nicer...
    And implemented methods for "giving/removing focus"

--- CustomKeyboard/KeyView.m
+++ CustomKeyboard/KeyView.m
@@ -12,6 +12,8 @@
 @interface KeyView ()
 @property (nonatomic) KeyboardKeyLayer* letterLayer;
 @property (nonatomic) CALayer* backgroundLayer;
+
+@property (nonatomic) BOOL hasFocus;
 @end
 
 @implementation KeyView
@@ -21,13 +23,7 @@
 {
    if (self = [super initWithFrame:frame])
    {
-      self.backgroundLayer = [CALayer layer];
-      self.backgroundLayer.backgroundColor = [UIColor whiteColor].CGColor;
-      self.backgroundLayer.cornerRadius = 12.f;
-      self.backgroundLayer.shadowOpacity = .1;
-      self.backgroundLayer.shadowOffset = CGSizeMake(0, -.5f);
-      
-      [self.layer addSublayer:self.backgroundLayer];
+      [self setupBackgroundLayer];
    }
    return self;
 }
@@ -62,19 +58,47 @@
    [self.layer addSublayer:self.letterLayer];
 }
 
+- (void)setupBackgroundLayer
+{
+   self.backgroundLayer = [CALayer layer];
+   self.backgroundLayer.backgroundColor = [UIColor whiteColor].CGColor;
+   self.backgroundLayer.cornerRadius = 4.f;
+   
+   self.backgroundLayer.shadowOpacity = .1;
+   self.backgroundLayer.shadowOffset = CGSizeMake(0, 1);
+   self.backgroundLayer.actions = @{@"frame" : [NSNull null],
+                                    @"position" : [NSNull null],
+                                    @"backgroundColor" : [NSNull null],
+                                    @"shadowRadius" : [NSNull null]};
+   
+   [self.layer addSublayer:self.backgroundLayer];
+}
+
 #pragma mark - Public
 - (void)giveFocus
 {
+   if (self.hasFocus == NO)
+   {
+      self.hasFocus = YES;
+      self.backgroundLayer.backgroundColor = [UIColor colorWithRed:.9 green:.9 blue:.9 alpha:1].CGColor;
+      self.backgroundLayer.shadowRadius = 0;
+   }
 }
 
 - (void)removeFocus
 {
+   if (self.hasFocus == YES)
+   {
+      self.hasFocus = NO;
+      self.backgroundLayer.backgroundColor = [UIColor whiteColor].CGColor;
+      self.backgroundLayer.shadowRadius = 3;
+   }
 }
 
 - (void)updateFrame:(CGRect)frame
 {
    self.frame = frame;
-   self.backgroundLayer.frame = CGRectInset(self.bounds, 1, 1);
+   self.backgroundLayer.frame = CGRectInset(self.bounds, 2, 4);
    self.letterLayer.position = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds));
 }
 
