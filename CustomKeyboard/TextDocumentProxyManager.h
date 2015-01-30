266ffd39b535a41ff6542205c469cecac5a76d7e
Gregory Klein <gregoryhklein@gmail.com>
2015-01-29 21:47:56 -0500

    + Singleton for managing the text document proxy

---
+++ CustomKeyboard/TextDocumentProxyManager.h
@@ -0,0 +1,13 @@
+//
+//  TextDocumentProxyManager.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/29/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TextDocumentProxyManager : NSObject
+
+@end
---
+++ CustomKeyboard/TextDocumentProxyManager.m
@@ -0,0 +1,13 @@
+//
+//  TextDocumentProxyManager.m
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/29/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "TextDocumentProxyManager.h"
+
+@implementation TextDocumentProxyManager
+
+@end
