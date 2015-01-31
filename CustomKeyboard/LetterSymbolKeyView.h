d96ff22b76e81a7c0d2551b2551843a070b14c4c
Gregory Klein <gregoryhklein@gmail.com>
2015-01-31 15:46:11 -0500

    Added subclass of KeyView for letters/symbols

---
+++ CustomKeyboard/LetterSymbolKeyView.h
@@ -0,0 +1,13 @@
+//
+//  LetterSymbolKeyView.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/31/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "KeyView.h"
+
+@interface LetterSymbolKeyView : KeyView
+
+@end
---
+++ CustomKeyboard/LetterSymbolKeyView.m
@@ -0,0 +1,21 @@
+//
+//  LetterSymbolKeyView.m
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/31/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "LetterSymbolKeyView.h"
+
+@implementation LetterSymbolKeyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
