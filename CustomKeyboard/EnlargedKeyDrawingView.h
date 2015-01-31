389df966761bc99edf6be56cc0cce100a2813336
Gregory Klein <gregoryhklein@gmail.com>
2015-01-31 18:01:53 -0500

    Added files for enlarged key drawing view

---
+++ CustomKeyboard/EnlargedKeyDrawingView.h
@@ -0,0 +1,13 @@
+//
+//  EnlargedKeyDrawingView.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/31/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnlargedKeyDrawingView : NSView
+
+@end
---
+++ CustomKeyboard/EnlargedKeyDrawingView.m
@@ -0,0 +1,13 @@
+//
+//  EnlargedKeyDrawingView.m
+//  SoundBoard
+//
+//  Created by Klein, Greg on 1/31/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "EnlargedKeyDrawingView.h"
+
+@implementation EnlargedKeyDrawingView
+
+@end
