97bfc52925b49f636aabee545cfa3d29333274ef
Gregory Klein <gregoryhklein@gmail.com>
2015-01-31 15:48:56 -0500

    Fixed call to [super class] in init method

--- CustomKeyboard/LetterSymbolKeyView.m
+++ CustomKeyboard/LetterSymbolKeyView.m
@@ -13,7 +13,7 @@
 
 + (instancetype)viewWithText:(NSString *)text fontSize:(CGFloat)fontSize frame:(CGRect)frame
 {
-   LetterSymbolKeyView* letterSymbolView = [super viewWithText:text fontSize:fontSize frame:frame];
+   LetterSymbolKeyView* letterSymbolView = [[super class] viewWithText:text fontSize:fontSize frame:frame];
    
    [letterSymbolView setActionBlock:^
    {
