80ba50301722ea328a7476f49c53a0f4c2fdc171
Gregory Klein <gregoryhklein@gmail.com>
2015-02-02 20:20:24 -0500

    Using enlarged key view in letter/symbol views

--- CustomKeyboard/LetterSymbolKeyView.m
+++ CustomKeyboard/LetterSymbolKeyView.m
@@ -8,20 +8,62 @@
 
 #import "LetterSymbolKeyView.h"
 #import "TextDocumentProxyManager.h"
+#import "EnlargedKeyView.h"
+
+@interface LetterSymbolKeyView ()
+@property (nonatomic) EnlargedKeyView* enlargedKeyView;
+@end
 
 @implementation LetterSymbolKeyView
 
+#pragma mark - Init
+- (instancetype)initWithFrame:(CGRect)frame
+{
+   if (self = [super initWithFrame:frame])
+   {
+      self.shouldShowEnlargedKeyOnTouchDown = YES;
+   }
+   return self;
+}
+
+#pragma mark - Class Init
 + (instancetype)viewWithText:(NSString *)text fontSize:(CGFloat)fontSize frame:(CGRect)frame
 {
    LetterSymbolKeyView* letterSymbolView = [super viewWithText:text fontSize:fontSize frame:frame];
-   letterSymbolView.shouldShowEnlargedKeyOnTouchDown = YES;
-   
+
+   [letterSymbolView setupEnlargedKeyView];
    [letterSymbolView setActionBlock:^
-   {
-      [TextDocumentProxyManager insertText:text];
-   }];
-   
+    {
+       [TextDocumentProxyManager insertText:text];
+    }];
+
    return letterSymbolView;
+}
+
+#pragma mark - Setup
+- (void)setupEnlargedKeyView
+{
+   self.enlargedKeyView = [EnlargedKeyView viewWithKeyView:self];
+   self.enlargedKeyView.hidden = YES;
+
+   [self addSubview:self.enlargedKeyView];
+}
+
+#pragma mark - Public
+- (void)updateFrame:(CGRect)frame
+{
+   [super updateFrame:frame];
+   [self.enlargedKeyView updateFrame:self.bounds];
+}
+
+- (void)giveFocus
+{
+   self.enlargedKeyView.hidden = NO;
+}
+
+- (void)removeFocus
+{
+   self.enlargedKeyView.hidden = YES;
 }
 
 @end
