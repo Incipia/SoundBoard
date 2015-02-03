96b7adb9daab2f89e34fc8936eaeb75a75b77d82
Gregory Klein <gregoryhklein@gmail.com>
2015-02-02 20:17:59 -0500

    Added new view for enlarged keys

---
+++ CustomKeyboard/EnlargedKeyView.h
@@ -0,0 +1,13 @@
+//
+//  EnlargedKeyView.h
+//  SoundBoard
+//
+//  Created by Gregory Klein on 2/2/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnlargedKeyView : UIView
+
+@end
---
+++ CustomKeyboard/EnlargedKeyView.m
@@ -0,0 +1,21 @@
+//
+//  EnlargedKeyView.m
+//  SoundBoard
+//
+//  Created by Gregory Klein on 2/2/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "EnlargedKeyView.h"
+
+@implementation EnlargedKeyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
