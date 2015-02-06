97797a24d58cbddf6706efd4706006ec39a82023
Gregory Klein <gregoryhklein@gmail.com>
2015-02-05 22:49:22 -0500

    Added files for LetterKeysController

---
+++ CustomKeyboard/LetterKeysController.h
@@ -0,0 +1,13 @@
+//
+//  LetterKeysController.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 2/5/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LetterKeysController : UIViewController
+
+@end
---
+++ CustomKeyboard/LetterKeysController.m
@@ -0,0 +1,37 @@
+//
+//  LetterKeysController.m
+//  SoundBoard
+//
+//  Created by Klein, Greg on 2/5/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "LetterKeysController.h"
+
+@interface LetterKeysController ()
+
+@end
+
+@implementation LetterKeysController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
