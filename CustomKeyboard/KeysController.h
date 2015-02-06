01271d4a670c7529eb3dbe63b83cd2f0eab59ea7
Gregory Klein <gregoryhklein@gmail.com>
2015-02-06 13:06:54 -0500

    Added files for KeyController base class and
    NumberSymbolKeysController

---
+++ CustomKeyboard/KeysController.h
@@ -0,0 +1,13 @@
+//
+//  KeysController.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 2/6/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeysController : UIViewController
+
+@end
