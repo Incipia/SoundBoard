2ff4636455892d2dc0880bae3c4d6a5fec88529b
Gregory Klein <gregoryhklein@gmail.com>
2015-02-09 00:37:34 -0500

    Added files for mode transitioner

---
+++ CustomKeyboard/KeyboardModeTransitioner.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardModeTransitioner.h
+//  SoundBoard
+//
+//  Created by Gregory Klein on 2/8/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeyboardModeTransitioner : NSObject
+
+@end
---
+++ CustomKeyboard/KeyboardModeTransitioner.m
@@ -0,0 +1,13 @@
+//
+//  KeyboardModeTransitioner.m
+//  SoundBoard
+//
+//  Created by Gregory Klein on 2/8/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "KeyboardModeTransitioner.h"
+
+@implementation KeyboardModeTransitioner
+
+@end
