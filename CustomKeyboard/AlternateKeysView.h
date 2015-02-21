51d1f3742966e8ca925f5c5f0337d6e7d7a675fc
Gregory Klein <gregoryhklein@gmail.com>
2015-02-21 17:10:07 -0500

    Added files for alternate keys view

---
+++ CustomKeyboard/AlternateKeysView.h
@@ -0,0 +1,13 @@
+//
+//  AlternateKeysView.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 2/21/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AlternateKeysView : NSObject
+
+@end
---
+++ CustomKeyboard/AlternateKeysView.m
@@ -0,0 +1,13 @@
+//
+//  AlternateKeysView.m
+//  SoundBoard
+//
+//  Created by Klein, Greg on 2/21/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "AlternateKeysView.h"
+
+@implementation AlternateKeysView
+
+@end
