61903c6416cede19d8fc2b7a2fbb5e1c575f8dae
Gregory Klein <gregoryhklein@gmail.com>
2015-02-23 13:06:45 -0500

    WIP - added string constants for attribute keys

--- CustomKeyboard/ThemeAttributesProvider.h
+++ CustomKeyboard/ThemeAttributesProvider.h
@@ -7,7 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "KeyboardTypedefs.h"
 
 @interface ThemeAttributesProvider : NSObject
 
++ (NSDictionary*)themeAttributesForKeyType:(KeyboardKeyType)type;
+
 @end
+
+extern NSString* const kThemeAttributesFontName;
+extern NSString* const kThemeAttributesFontSize;
+extern NSString* const kThemeAttributesFontColor;
+extern NSString* const kThemeAttributesBackgroundColor;
+extern NSString* const kThemeAttributesCornerRadius;
+extern NSString* const kThemeAttributesShadowColor;
+extern NSString* const kThemeAttributesShadowOffset;
+extern NSString* const kThemeAttributesShadowOpacity;
+extern NSString* const kThemeAttributesShadowRadius;
+extern NSString* const kThemeAttributesbackgroundEdgeInsets;
--- CustomKeyboard/ThemeAttributesProvider.m
+++ CustomKeyboard/ThemeAttributesProvider.m
@@ -8,6 +8,22 @@
 
 #import "ThemeAttributesProvider.h"
 
+NSString* const kThemeAttributesFontName = @"kThemeAttributesFontName";
+NSString* const kThemeAttributesFontSize = @"kThemeAttributesFontSize";
+NSString* const kThemeAttributesFontColor = @"kThemeAttributesFontColor";
+NSString* const kThemeAttributesBackgroundColor = @"kThemeAttributesBackgroundColor";
+NSString* const kThemeAttributesCornerRadius = @"kThemeAttributesCornerRadius";
+NSString* const kThemeAttributesShadowColor = @"kThemeAttributesShadowColor";
+NSString* const kThemeAttributesShadowOffset = @"kThemeAttributesShadowOffset";
+NSString* const kThemeAttributesShadowOpacity = @"kThemeAttributesShadowOpacity";
+NSString* const kThemeAttributesShadowRadius = @"kThemeAttributesShadowRadius";
+NSString* const kThemeAttributesbackgroundEdgeInsets = @"kThemeAttributesbackgroundEdgeInsets";
+
 @implementation ThemeAttributesProvider
+
++ (NSDictionary*)themeAttributesForKeyType:(KeyboardKeyType)type
+{
+   return nil;
+}
 
 @end
