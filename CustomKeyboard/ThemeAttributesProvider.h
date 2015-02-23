711a892b084d8d200b728703a52dda2c7aaf3152
Gregory Klein <gregoryhklein@gmail.com>
2015-02-23 13:06:09 -0500

    Added files for theme attributes provider

---
+++ CustomKeyboard/ThemeAttributesProvider.h
@@ -0,0 +1,13 @@
+//
+//  ThemeAttributesProvider.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 2/23/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThemeAttributesProvider : NSObject
+
+@end
---
+++ CustomKeyboard/ThemeAttributesProvider.m
@@ -0,0 +1,13 @@
+//
+//  ThemeAttributesProvider.m
+//  SoundBoard
+//
+//  Created by Klein, Greg on 2/23/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "ThemeAttributesProvider.h"
+
+@implementation ThemeAttributesProvider
+
+@end
