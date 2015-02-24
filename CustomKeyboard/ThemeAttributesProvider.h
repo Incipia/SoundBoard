a5a495398f78bc0184469740034306fd99cbc514
Gregory Klein <gregoryhklein@gmail.com>
2015-02-23 21:52:05 -0500

    First pass implementation for theme attrs provider

--- CustomKeyboard/ThemeAttributesProvider.h
+++ CustomKeyboard/ThemeAttributesProvider.h
@@ -6,22 +6,42 @@
 //  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import "KeyboardTypedefs.h"
 
 @interface ThemeAttributesProvider : NSObject
 
-+ (NSDictionary*)themeAttributesForKeyType:(KeyboardKeyType)type;
++ (NSDictionary*)attributesForKeyType:(KeyboardKeyType)type;
+
++ (NSString*)fontNameForKeyType:(KeyboardKeyType)type;
++ (CGFloat)fontSizeForKeyType:(KeyboardKeyType)type;
++ (UIColor*)fontColorForKeyType:(KeyboardKeyType)type;
++ (UIColor*)highlightedFontColorForKeyType:(KeyboardKeyType)type;
++ (UIColor*)backgroundColorForKeyType:(KeyboardKeyType)type;
++ (UIColor*)highlightedBackgroundColorForKeyType:(KeyboardKeyType)type;
++ (CGFloat)cornerRadiusForKeyType:(KeyboardKeyType)type;
++ (UIColor*)shadowColorForKeyType:(KeyboardKeyType)type;
++ (CGSize)shadowOffsetForKeyType:(KeyboardKeyType)type;
++ (CGFloat)shadowOpacityForKeyType:(KeyboardKeyType)type;
++ (CGFloat)shadowRadiusForKeyType:(KeyboardKeyType)type;
++ (UIEdgeInsets)backgroundEdgeInsetsForKeyType:(KeyboardKeyType)type;
++ (UIColor*)borderColorForKeyType:(KeyboardKeyType)type;
++ (CGFloat)borderWidthForKeyType:(KeyboardKeyType)type;
 
 @end
 
 extern NSString* const kThemeAttributesFontName;
 extern NSString* const kThemeAttributesFontSize;
 extern NSString* const kThemeAttributesFontColor;
+extern NSString* const kThemeAttributesHighlightedFontColor;
 extern NSString* const kThemeAttributesBackgroundColor;
+extern NSString* const kThemeAttributesHighlightedBackgroundColor;
+extern NSString* const kThemeAttributesBackgroundEdgeInsets;
 extern NSString* const kThemeAttributesCornerRadius;
 extern NSString* const kThemeAttributesShadowColor;
 extern NSString* const kThemeAttributesShadowOffset;
 extern NSString* const kThemeAttributesShadowOpacity;
 extern NSString* const kThemeAttributesShadowRadius;
-extern NSString* const kThemeAttributesbackgroundEdgeInsets;
+extern NSString* const kThemeAttributesBorderColor;
+extern NSString* const kThemeAttributesBorderWidth;
+
