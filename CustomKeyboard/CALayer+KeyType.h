d15a1e16d72302651d4cab94cbb312cf77843415
Gregory Klein <gregoryhklein@gmail.com>
2015-02-23 21:51:29 -0500

    Added files for CALayer category

---
+++ CustomKeyboard/CALayer+KeyType.h
@@ -0,0 +1,13 @@
+//
+//  CALayer+KeyType.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 2/23/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface CALayer (KeyType)
+
+@end
---
+++ CustomKeyboard/CALayer+KeyType.m
@@ -0,0 +1,13 @@
+//
+//  CALayer+KeyType.m
+//  SoundBoard
+//
+//  Created by Klein, Greg on 2/23/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "CALayer+KeyType.h"
+
+@implementation CALayer (KeyType)
+
+@end
