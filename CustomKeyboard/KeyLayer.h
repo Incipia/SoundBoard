2606d9ccd1de247a3833626c76dc56118a135efc
Gregory Klein <gregoryhklein@gmail.com>
2015-02-24 01:38:03 -0500

    Added files for CAShapeLayer subclass

---
+++ CustomKeyboard/KeyLayer.h
@@ -0,0 +1,13 @@
+//
+//  KeyLayer.h
+//  SoundBoard
+//
+//  Created by Klein, Greg on 2/24/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface KeyLayer : CAShapeLayer
+
+@end
---
+++ CustomKeyboard/KeyLayer.m
@@ -0,0 +1,13 @@
+//
+//  KeyLayer.m
+//  SoundBoard
+//
+//  Created by Klein, Greg on 2/24/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "KeyLayer.h"
+
+@implementation KeyLayer
+
+@end
