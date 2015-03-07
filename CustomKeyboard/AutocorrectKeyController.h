41135e1cc6eaf78089c0b03e76e821851d784a88
Gregory Klein <gregoryhklein@gmail.com>
2015-03-07 12:41:51 -0500

    Added files for AutocorrectKeyController

---
+++ CustomKeyboard/AutocorrectKeyController.h
@@ -0,0 +1,13 @@
+//
+//  AutocorrectKeyController.h
+//  SoundBoard
+//
+//  Created by Gregory Klein on 3/7/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AutocorrectKeyController : UIViewController
+
+@end
---
+++ CustomKeyboard/AutocorrectKeyController.m
@@ -0,0 +1,37 @@
+//
+//  AutocorrectKeyController.m
+//  SoundBoard
+//
+//  Created by Gregory Klein on 3/7/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "AutocorrectKeyController.h"
+
+@interface AutocorrectKeyController ()
+
+@end
+
+@implementation AutocorrectKeyController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
