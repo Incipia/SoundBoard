a6cf26b52fd75232872191f6b12648183a0f0ae3
Gregory Klein <gregoryhklein@gmail.com>
2015-03-14 17:34:10 -0400

    Added files:
    - AutocorrectKeyManager
    - UITextChecker+Additions

---
+++ CustomKeyboard/AutocorrectKeyManager.m
@@ -0,0 +1,13 @@
+//
+//  AutocorrectKeyManager.m
+//  SoundBoard
+//
+//  Created by Gregory Klein on 3/14/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "AutocorrectKeyManager.h"
+
+@implementation AutocorrectKeyManager
+
+@end
