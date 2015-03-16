00eb6222c03d2f2255917531dc8c4b96b9c66dd4
Gregory Klein <gregoryhklein@gmail.com>
2015-03-15 20:40:28 -0400

    Added some files

---
+++ CustomKeyboard/SpellCorrectionResult.h
@@ -0,0 +1,13 @@
+//
+//  SpellCorrectionResult.h
+//  SoundBoard
+//
+//  Created by Gregory Klein on 3/15/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SpellCorrectionResult : NSObject
+
+@end
---
+++ CustomKeyboard/SpellCorrectionResult.m
@@ -0,0 +1,13 @@
+//
+//  SpellCorrectionResult.m
+//  SoundBoard
+//
+//  Created by Gregory Klein on 3/15/15.
+//  Copyright (c) 2015 Pure Virtual Studios, LLC. All rights reserved.
+//
+
+#import "SpellCorrectionResult.h"
+
+@implementation SpellCorrectionResult
+
+@end
